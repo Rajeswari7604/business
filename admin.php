e2164b4f21d7875807c20032b9f621eacd68c635
Rajeswari7604 <rajeswarip632@gmail.com>
2024-09-13 19:42:07 +0530

    first commit

---
+++ admin.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="form" >
+    
+    <div class="wrapper">
+        
+        <form action="#">
+            <h2>LOGIN FORM</h2>
+            <div class="inputbox">
+                <label for="username"></label>
+                <input type="text"  placeholder="Enter a Email id " required>
+            </div>
+            <div class="inputbox">
+                <label for="password"></label>
+                <input type="password" placeholder="Enter a password" required>
+            </div>
+            <div class="forgot">
+                <label ><input type="checkbox">Forgot Password</label>
+            </div>
+            <button type="submit" class="btn">
+                <a href="hello.html">Login</a>
+            </button>
+        </form>
+    </div>
+    </div>
+</body>
+</html>
